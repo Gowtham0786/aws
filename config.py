c37adb998fb2549a09aaf84e74cd64c349058404
Gowtham0786 <54531133+Gowtham0786@users.noreply.github.com>
2020-08-09 21:47:07 +0530

    Initial Commit for Deployment
    Initial Commit for Deployment

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "gowthamdb.cc24md17kkkm.us-east-2.rds.amazonaws.com"
+customuser = "admin"
+custompass = "root1234"
+customdb = "gowthamdb"
+custombucket = "gowtham-us-east"
+customregion = "us-east-2"
